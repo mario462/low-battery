a7ed21b5ec22e195c134768a19c487d20dbcc79c
Mario Muniz <mario462@gmail.com>
2017-01-21 00:05:02 -0500

    The install script receives the user for which the service will run
    The service, when running as root, was not able to play the desired sounds, so the service needs to be run by a regular user that the install script will receive as a parameter and the fill in the service file

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+  echo "Bad usage: install.sh user"
+  echo "user: the user for which you want to install the service"
+  exit 1
+fi
+
 service='low-battery'
 chmod +x "$service"
 cp ./${service}.conf /etc/
 mkdir -p /usr/share/low-battery
 cp ./*.wav /usr/share/low-battery
 cp ./$service /usr/local/bin/$service
-cp ./$service.service /lib/systemd/system/$service.service
+sed "s/USER/$1/" < $service.service > /lib/systemd/system/$service.service
 systemctl enable "$service"
 systemctl start "$service"
